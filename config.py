5f854b9e9be5dea112128c52f34e859ab4cebc9d
kunalkushwahatg <kunalkushwahatg@gmail.com>
2024-06-01 02:47:19 -0700

    config created and minor changes

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
- 
+#creates neccessary config parameters 
+N_GRIDS = 7
+N_BOXES = 2
+N_CLASSES = 20
+IMAGE_SIZE = 224
+DEVICE = 'cuda'
+BATCH_SIZE = 64
--- utils/create_data.py
+++ utils/create_data.py
@@ -1,11 +1,11 @@
 import numpy as np
 import os
 from tqdm import tqdm_gui
-from get_labels import GetLabel
+from get_labels import Label
 import pickle 
 
-images_path = "C:/Users/kunalkushwahatg/Desktop/yolov1_from_scratch/data/VOC2012_train_val/VOC2012_train_val/JPEGImages/"
-annotations_path = "C:/Users/kunalkushwahatg/Desktop/yolov1_from_scratch/data/VOC2012_train_val/VOC2012_train_val/Annotations/"
+images_path = "E:/VOC2012_train_val/VOC2012_train_val/JPEGImages/"
+annotations_path = "E:/VOC2012_train_val/VOC2012_train_val/Annotations/"
 
 dataset = []
 
@@ -14,14 +14,11 @@ for i,image_path in tqdm_gui(enumerate(os.listdir(images_path))):
     annotation_filename = image_path.split(".")[0]+".xml"
     image_path = images_path+image_path
     annotation_path = annotations_path+annotation_filename
-    gl = GetLabel(n_grids=7,n_boxes=2)  
+    gl = Label(n_grids=7,n_boxes=2)  
     try:
         image , label = gl.get_label(image_path,annotation_path)
+        dataset.append([image,label])
     except Exception as e:
         pass
-    dataset.append([image,label])
-
-
-
-with open("C:/Users/kunalkushwahatg/Desktop/yolov1_from_scratch/data/dataset.pickle","wb") as f:
-    pickle.dump(dataset,f)
+    
+pickle.dump(dataset,open("dataset1.pkl","wb"))
